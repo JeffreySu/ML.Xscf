ef3d43e3b07114b1ec5f42ed9500cbaada374e47
JeffreySu <www.jeffrey.su@gmail.com>
2020-05-30 17:59:59 +0800

    添加单元测试

---
+++ ML.Xscf.Tests/BaseTest.cs
@@ -0,0 +1,13 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace ML.Xscf.Tests
+{
+    [TestClass]
+    public class BaseTest
+    {
+        public BaseTest()
+        {
+
+        }
+    }
+}
---
+++ ML.Xscf.Tests/Functions/UpdateDocsTests.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ML.Xscf.Tests.Functions
+{
+    [TestClass]
+    public class UpdateDocsTests
+    {
+        [TestMethod]
+        public void RunTest()
+        {
+
+        }
+    }
+}
